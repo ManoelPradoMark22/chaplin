696798bd8e46c806a3947483e7231aaa98b2dc24
unknown <manoelprado.aecjr@gmail.com>
2021-09-08 11:55:26 -0300

    start shopCart creation

--- script.js
+++ script.js
@@ -67,6 +67,26 @@ slider.addEventListener("mousemove", e => {
   slider.scrollLeft = scrollLeft - walk;
 });
 
+/*Cart functions*/
+
+function closeCart() {
+	const cart = document.querySelector('.producstOnCart');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling')
+}
+
+const openShopCart = document.querySelector('.bagDiv');
+openShopCart.addEventListener('click', () => {
+	const cart = document.querySelector('.producstOnCart');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling');
+});
+
+const closeShopCart = document.querySelector('#closeButton');
+const overlay = document.querySelector('.overlay');
+closeShopCart.addEventListener('click', closeCart);
+overlay.addEventListener('click', closeCart);
+
 /*CATEGORIES*/
 
 /*SESSÃO 1
